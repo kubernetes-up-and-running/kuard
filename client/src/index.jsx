ed77ef8890910758af1cf16375ae543e0ddb1bea
Joe Beda <joe.github@bedafamily.com>
2017-01-22 20:06:47 -0800

    Convert FS to React

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,11 @@ import Details from './details';
 import Probe from './probe'
 import Env from './env'
 
-render(<Probe title="Liveness Check" path="/healthy" />, document.getElementById('liveness'));
-render(<Probe title="Readiness Check" path="/ready" />, document.getElementById('readiness'));
-render(<Env path="/env" />, document.getElementById('env'));
+render(<Probe title="Liveness Check" path="/healthy" />, document.getElementById("liveness"));
+render(<Probe title="Readiness Check" path="/ready" />, document.getElementById("readiness"));
+render(<Env path="/env" />, document.getElementById("env"));
+
+render(
+  <Details title="File System">
+      <a href="/fs/">Browse the root file system for this server.</a>
+  </Details>, document.getElementById("fs"))
